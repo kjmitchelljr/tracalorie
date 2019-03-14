c58e5b5e3902a89121e373f5f808a285430dfd43
KJ Mitchell <kajewebdevm@gmail.com>
2019-03-14 16:15:11 -0400

    User is able to remove an item from the form

--- app.js
+++ app.js
@@ -67,6 +67,16 @@ const ItemCtrl = (function() {
 
       return found;
     },
+    deleteItem: function(id) {
+      // Get ids
+      const ids = state.items.map(item => item.id);
+
+      // Get index
+      const index = ids.indexOf(id);
+
+      // Remove Item
+      state.items.splice(index, 1);
+    },
     setCurrentItem: function(item) {
       state.currentItem = item;
     },
@@ -149,6 +159,9 @@ const UICtrl = (function() {
         <a href="#" class="secondary-content">
           <i class="edit-item fa fa-pencil"></i>
         </a>`;
+    },
+    deleteListItem: function(id) {
+      document.querySelector(`#item-${id}`).remove();
     },
     clearInput: function() {
       document.querySelector(UISelectors.itemNameInput).value = '';
@@ -249,7 +262,7 @@ const App = (function(ItemCtrl, UICtrl) {
       e.preventDefault();
     });
 
-    // Update Item on submit
+    // Update Item event
     document.querySelector(UISelectors.updateBtn).addEventListener('click', (e) => {
       // Get Item input
       const input = UICtrl.getItemInput();
@@ -270,6 +283,31 @@ const App = (function(ItemCtrl, UICtrl) {
 
       e.preventDefault();
     });
+
+    // Delete item event
+    document.querySelector(UISelectors.deleteBtn).addEventListener('click', (e) => {
+      // Get current item
+      const currentItem = ItemCtrl.getCurrentItem();
+
+      // Delete from data structure
+      ItemCtrl.deleteItem(currentItem.id);
+
+      // Delete from the UI
+      UICtrl.deleteListItem(currentItem.id);
+
+      // Get Total Calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
+      UICtrl.clearEditState();
+
+      e.preventDefault();
+    });
+
+    // Back button event
+    document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState);
   }
 
   // Public Methods
